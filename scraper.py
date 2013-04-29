abe6464c2700f84f59ecb7fc82cebe7dd5c989ba
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-04-29 10:12:26 -0400

    working with monthly url

--- scraper.py
+++ scraper.py
@@ -14,7 +14,8 @@ with open('cities', 'r') as csvfile:
 		airports.append({"airport": row[0], "city": row[1], "state": row[2]})
 
 def urlForAirportAndYear(airport, year):
-	return "http://www.wunderground.com/history/airport/" + airport + "/" + str(year) + "/1/01/CustomHistory.html?dayend=01&monthend=1&yearend=" + str(year+1)
+	# return "http://www.wunderground.com/history/airport/" + airport + "/" + str(year) + "/1/01/CustomHistory.html?dayend=01&monthend=1&yearend=" + str(year+1)
+	return "http://www.wunderground.com/history/airport/" + airport + "/" + str(year) + "/1/1/MonthlyHistory.html"
 
 def fetchWeatherDataForAirportAndYear(airport, year):
 	try:
@@ -24,21 +25,21 @@ def fetchWeatherDataForAirportAndYear(airport, year):
 		avg_temp = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[3].by_class("b")[1].content
 		avg_max_temp = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[2].by_class("b")[1].content
 		avg_min_temp = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[4].by_class("b")[1].content
-		avg_dew_point = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[10].by_class("b")[1].content
-		avg_precipitation = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[12].by_class("b")[1].content
-		avg_wind = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[15].by_class("b")[1].content
-		avg_gust_wind = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[16].by_class("b")[1].content
-		avg_sea_level_pressure = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[18].by_class("b")[1].content
+		# avg_dew_point = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[10].by_class("b")[1].content
+		# avg_precipitation = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[12].by_class("b")[1].content
+		# avg_wind = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[15].by_class("b")[1].content
+		# avg_gust_wind = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[16].by_class("b")[1].content
+		# avg_sea_level_pressure = dom.by_class("contentData")[0].by_tag("table")[0].by_tag("tr")[18].by_class("b")[1].content
 
 		print "good"	
 		return {"avg_temp": avg_temp, 
 			"avg_min_temp": avg_min_temp, 
 			"avg_max_temp": avg_max_temp, 
-			"avg_dew_point": avg_dew_point, 
-			"avg_precipitation": avg_precipitation, 
-			"avg_wind": avg_wind,
-			"avg_gust_wind": avg_gust_wind, 
-			"avg_sea_level_pressure": avg_sea_level_pressure
+			# "avg_dew_point": avg_dew_point, 
+			# "avg_precipitation": avg_precipitation, 
+			# "avg_wind": avg_wind,
+			# "avg_gust_wind": avg_gust_wind, 
+			# "avg_sea_level_pressure": avg_sea_level_pressure
 		}
 	except URLTimeout:
 		print "timeout"
@@ -46,7 +47,7 @@ def fetchWeatherDataForAirportAndYear(airport, year):
 
 yearData = {}
 
-for i in range(1970, 2013):
+for i in range(2000, 2013):
 	yearData[i] = []
 	for j in airports: 
 		try:
@@ -56,6 +57,6 @@ for i in range(1970, 2013):
 			continue
 
 
-f = open("js/data8.json", "w")
+f = open("data/data8.json", "w")
 f.write(json.dumps(yearData))
 f.close()
