6d08011d9b86b958bc2f4e79c676c406b0ba1292
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-05-01 10:22:10 -0400

    finished data combining tools

--- html/js/dataCombine.js
+++ html/js/dataCombine.js
@@ -14,7 +14,7 @@ window.onload = function() {
         return target;
     }
 
-    var finalJSON = mergeJson({}, data, data1);
+    var finalJSON = mergeJson({}, data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11);
 
     console.log(JSON.stringify(finalJSON));
 };
