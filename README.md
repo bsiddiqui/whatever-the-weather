a0e7690da0f21ba181347e65c623fdeccd539285
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-05-02 14:49:50 -0400

    added cities updated TODO

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 A weather visualization by Basil Siddiqui and Kevin Schmid
 
-
 TODO:
-- add data scraping info to process book
 - refactor code for new data (currently in dataCombined.json)
 - add table visual encoding
 - consider overlaying avg_temp for US line chart under the slider
--- html/js/dataCombine.js
+++ html/js/dataCombine.js
@@ -14,7 +14,7 @@ window.onload = function() {
         return target;
     }
 
-    var finalJSON = mergeJson({}, data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11);
+    var finalJSON = mergeJson({}, data0, data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14);
 
     console.log(JSON.stringify(finalJSON));
 };
